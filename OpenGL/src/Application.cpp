b1097f8a0916d037fcd8977cebd78bcb35be6e57
TalaatHarb <mohamed.talaatharb@eng.asu.ed.eg>
2017-10-08 14:29:03 +0200

    - Adding details relating to rendering a vertex buffer (as comments until finished)

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+#define WIDTH 800
+#define HEIGHT 600
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -12,12 +15,16 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
+	window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
+
+	/* Center window */
+	const GLFWvidmode* vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+	glfwSetWindowPos(window, (vidMode->width - WIDTH) / 2, (vidMode->height - HEIGHT) / 2);
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
@@ -27,6 +34,17 @@ int main(void)
 		return -1;
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	/* Build vertex buffer */
+	//float posistions[6] = {
+	//	-0.5f, -0.5f,
+	//	 0.0f,  0.5f,
+	//	 0.5f, -0.5f
+	//};
+	//unsigned int vBuffer;
+	//glGenBuffers(1, &vBuffer);
+	//glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
+	//glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), posistions, GL_STATIC_DRAW);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -40,6 +58,9 @@ int main(void)
 		glVertex2f(0.0f, 0.5f);
 		glVertex2f(0.5f, -0.5f);
 		glEnd();
+
+		/* Rendering using vertex buffer */
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
